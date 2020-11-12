98d644f4c6e57c3f23c6bd79e97ab6361c7e21f7
rbhushan95 <rbhushan95@gmail.com>
2020-11-12 21:26:51 +0530

    New Practice application added

---
+++ Algorithm/Naggaro/ConvertJsVariableToCSharp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algorithm.Naggaro
+{
+    //input first_name_value,lst_date_for_caller
+    //outout first_Name_Value,lst_Date_For_Caller
+    public class ConvertJsVariableToCSharp
+    {
+        
+    }
+}
---
+++ AlgorithmTest/Test_ConvertJsVariableToCSharp.cs
@@ -0,0 +1,30 @@
+﻿using Xunit;
+
+namespace AlgorithmTest
+{
+    public class TestLogic
+    {
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            bool result = true;
+            Assert.False(result, "1 should not be prime");
+        }
+        [Fact]
+        public void AlwaysTrue()
+        {
+            Assert.True(true);
+        }
+        [Fact]
+        public void AlwaysFalse()
+        {
+            Assert.True(false);
+        }
+        [Fact]
+        public void ItDepends()
+        {
+            bool result = true;
+            Assert.False(result, "Need Logic");
+        }
+    }
+}
