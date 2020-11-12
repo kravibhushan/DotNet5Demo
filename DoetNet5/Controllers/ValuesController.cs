eaed39cbfc11bb88c8c2b7f286a3b754eb60caa6
rbhushan95 <rbhushan95@gmail.com>
2020-11-12 20:29:23 +0530

    Initial checkin

---
+++ DoetNet5/Controllers/ValuesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotNet5.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public List<string> get() 
+        {
+            List<string> lstString = new List<string>() { "Value1", "Value2", "Value3" };
+            return lstString;
+        }
+    }
+}
---
+++ Oops/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+
+//namespace Opps
+//{
+//    class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            WriteLine("Hello World!");
+//        }
+//    }
+//}
+
+WriteLine("Hello World!");
